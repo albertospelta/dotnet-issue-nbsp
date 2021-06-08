132e78bbeaff4efdea88dcaa8071dd62ffce5010
Alberto Spelta <alberto.spelta@live.com>
2021-06-08 09:04:21 +0200

    Add project files.

---
+++ ConsoleAppNET50/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleAppNET50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = System.IO.File.ReadAllText(@"Model.bim");
+            _ = Microsoft.AnalysisServices.JsonSerializer.DeserializeDatabase(json);
+        }
+    }
+}
---
+++ ConsoleAppNETCore31/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleAppNETCore31
+{
+    class Program
+    {
+        static void Main()
+        {
+            var json = System.IO.File.ReadAllText(@"Model.bim");
+            _ = Microsoft.AnalysisServices.JsonSerializer.DeserializeDatabase(json);
+        }
+    }
+}
